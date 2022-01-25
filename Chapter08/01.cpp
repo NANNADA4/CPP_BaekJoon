10d77155985e7a74b513fac38d5a69a79a085d8f
NANNADA4 <nannada4@icloud.com>
2022-01-25 09:13:39 +0900

    -

--- Chapter08/01.cpp
+++ Chapter08/01.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
+#include <iostream>
+#include <ostream>
 using namespace std;
 
 int main() {
     int a, b, c, date;
 
     cin >> a >> b >> c;
+
+    cout << "hello, World!" << endl;
+
+    return 0;
 }
